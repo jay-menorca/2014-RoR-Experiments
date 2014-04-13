8d5880b34d06852c45083010bc38a026b8d33248
jay-menorca <zaimenorca@gmail.com>
2014-04-13 17:42:28 +0800

    added test application (uses postgres)

---
+++ jaysBlog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JaysBlog::Application.config.secret_key_base = '1654f4d9b227890c3b137164e4d7beadb2481476ac1eb152ef618b5be3aad6d8651c166b3ab12284ef76410d464820ee73b69c96aaabcf301fd7a5e4601c9d00'
