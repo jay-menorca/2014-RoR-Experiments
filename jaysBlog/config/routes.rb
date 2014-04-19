3e04fd145e002b86317c833c839981d7d00a75d5
jay-menorca <zaimenorca@gmail.com>
2014-04-19 17:33:40 +0800

    added comments association and functionality

--- jaysBlog/config/routes.rb
+++ jaysBlog/config/routes.rb
@@ -5,7 +5,9 @@ JaysBlog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
 
